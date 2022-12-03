aad5bf377b3ae2e051baab9eb4b6bc488be3ce9d
Andreas <andreas@wachs.dk>
2022-12-03 16:54:21 +0100

    Bootstrap

---
+++ 2022/3_rucksack_reorganization/main.go
@@ -0,0 +1,7 @@
+package main
+// Use paralellism for fun: https://levelup.gitconnected.com/how-to-implement-concurrency-and-parallelism-in-go-83c9c453dd2
+var wg sync.WaitGroup
+
+func main() {
+
+}
