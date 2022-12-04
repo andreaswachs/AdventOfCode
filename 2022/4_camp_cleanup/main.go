cbd7b59ae3308dead7c94ed4a6cbf247cc1e5e2a
Andreas Wachs <andreas@wachs.dk>
2022-12-04 08:14:15 +0100

    2022/4 (#5)
    * Bootstrap
    
    * Solve Camp Cleanup
    
    * Remove go module file and input files
    
    * Fix comment
    
    * Add some comments

---
+++ 2022/4_camp_cleanup/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// The Range struct models a range with inclusive bounds
+type Range struct {
+	From int
+	To   int
+}
+
+// Evalutates whether or not the range `r` contains another range `otherRange`.
+// Keep in mind that even if one Range does not contain the other, the other
+// might contain the first one, so you might need to check "in both directions".
+func (r *Range) contains(otherRange Range) bool {
+	return otherRange.From >= r.From && otherRange.To <= r.To
+}
+
+// Computes whether there is any overlap between two ranges
+func (r *Range) overlaps(otherRange Range) bool {
+
+	// You should be able to compute this in constant time instead by
+	// comparing From/To's, but I'm speedrunning this and this is
+	// way faster for me to express. Inputs are also range at a maximum of 1-100ish,
+	// so the input size is rather small
+	for i := r.From; i <= r.To; i++ {
+		if i >= otherRange.From && i <= otherRange.To {
+			// Returns early, if there are overlap
+			return true
+		}
+	}
+
+	return false
+}
+
+// Creates a new range from a line of the input.
+// Line is expected to be of shape: "from-to", e.g.: "1-3"
+func NewRange(line string) Range {
+	fromTo := strings.Split(line, "-")
+
+	// Convert From and To to integers, panic if we encounter an error as input _should_ be perfect
+	from, err := strconv.Atoi(fromTo[0])
+	if err != nil {
+		panic(err)
+	}
+
+	to, err := strconv.Atoi(fromTo[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return Range{From: from, To: to}
+}
+
+// Solves for part 1
+func solve1(line string) int {
+	// The line should be of shape "<<range>>,<<range>>"
+	ranges := strings.Split(line, ",")
+	leftRange := NewRange(ranges[0])
+	rightRange := NewRange(ranges[1])
+
+	if leftRange.contains(rightRange) || rightRange.contains(leftRange) {
+		return 1
+	}
+
+	return 0
+}
+
+func solve2(line string) int {
+	// The line should be of shape "<<range>>,<<range>>"
+	ranges := strings.Split(line, ",")
+	leftRange := NewRange(ranges[0])
+	rightRange := NewRange(ranges[1])
+
+	if leftRange.overlaps(rightRange) {
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	file, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	var result1 int
+	var result2 int
+
+	lines := strings.Split(string(file), "\n")
+	if lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1] // Drop the last element if its empty
+	}
+
+	for _, line := range lines {
+		result1 += solve1(line)
+		result2 += solve2(line)
+	}
+
+	fmt.Printf("Part 1 result: %d\n", result1)
+	fmt.Printf("Part 2 result: %d\n", result2)
+}
