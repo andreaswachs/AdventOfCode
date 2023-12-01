37b6fc721d3f02722049dd09b91c9b199e9a7a8a
Andreas Wachs <andreas@wachs.dk>
2023-12-01 20:43:11 +0100

    Add comments

--- 2023/01/Program.cs
+++ 2023/01/Program.cs
@@ -8,10 +8,15 @@
     };
 
     private static IEnumerable<string> GetForwardEnumerableOf(string s) {
+        // We find a maximum window size of either 5 or the length of the string
         int right = s.Length > 5 ? 5 : s.Length;
         for(int left = 0; left < s.Length; left++) {
+
+            // Yield the substring  
             yield return s.Substring(left, right - left);
 
+            // If the window has not reached the right side, we move the window
+            // to the right
             if (right < s.Length) {
                 right++;
             }
@@ -19,36 +24,46 @@
     }
 
     private static IEnumerable<string> GetBackwardEnumerableOf(string s) {
+        // Move a window through the string from the right to the left
+        // with variable length from 1 to 5
         for(int left = s.Length - 1, right = s.Length; right >= 1;) {
+
+            // Yield the substring
             yield return s.Substring(left, right - left);
+
+            // If the window has reached the left side, we tighten the window from the right
             if (left == 0) {
                 right--;
             } 
+
+            // If we have not reached the left side, we move the window
             if (left > 0) {
                 left--;
             }
+
+            // If the window is too large, we tighten it from the right
             if (right - left > 5) {
                 right--;
             }
         }
     }
-    private static int stringDigitToInt(string s)
-        => Array.IndexOf(digits, s) + 1;
 
-    private static int get(IEnumerable<string> e)
+    private static int GetFirstDigitInDirection(IEnumerable<string> directionEnumerable)
     {
-        foreach(string window in e)
+        foreach(string window in directionEnumerable)
         {
+            // First we check if we have reached a "simple" digit, i.e. 0-9
             if (Char.IsDigit(window[0]))
             {
                 return (int) window[0] - '0';
             }
 
+            // We now check if we have reached a word digit, i.e. one, two, three, ...
             foreach(var digit in digits)
             {
                 if (window.StartsWith(digit))
                 {
-                    return stringDigitToInt(digit);
+                    return Array.IndexOf(digits, window) + 1;
                 }
             }
 
@@ -75,8 +90,8 @@
                 var forward = GetForwardEnumerableOf(s);
                 var backward = GetBackwardEnumerableOf(s);
 
-                var a = get(forward) * 10;
-                var b = get(backward);
+                var a = GetFirstDigitInDirection(forward) * 10;
+                var b = GetFirstDigitInDirection(backward);
 
                 sum += a + b;
             }
