262116a1ed2201913a88846d9915cc98f0d59a7d
Andreas Wachs <andreas@wachs.dk>
2023-12-03 12:05:41 +0100

    Finish part2

--- 2023/03/Program.cs
+++ 2023/03/Program.cs
@@ -1,4 +1,19 @@
-﻿internal class PartsNumber
+﻿internal class Utils
+{
+    public static Func<(int, int), bool> WithinBounds(int maxx, int maxy)
+    {
+        Func<(int, int), bool> pred = pos => {
+            return pos.Item1 >= 0 
+                && pos.Item1 < maxx 
+                && pos.Item2 >= 0 
+                && pos.Item2 <= maxy;
+        };
+
+        return pred;
+    }
+}
+
+internal class PartsNumber
 {
     public int Value { get; set; }
     public int X { get; set; }
@@ -38,20 +53,10 @@
         positions.Add((this.X - 1, this.Y));
         positions.Add((this.X + length, this.Y));
 
-        return positions.Where(_withinBounds(maxX, maxY));
+        return positions.Where(Utils.WithinBounds(maxX, maxY));
     }
 
-    private Func<(int, int), bool> _withinBounds(int maxx, int maxy)
-    {
-        Func<(int, int), bool> pred = pos => {
-            return pos.Item1 >= 0 
-                && pos.Item1 < maxx 
-                && pos.Item2 >= 0 
-                && pos.Item2 <= maxy;
-        };
 
-        return pred;
-    }
 }
 
 internal class ProgramInput
@@ -134,7 +139,6 @@ internal class Program
         return new ProgramInput(gridx, gridy, partsNumbers, grid);
     }
 
-
     private static void Part1(ProgramInput input)
     {
         var part1 = 0;
@@ -155,11 +159,110 @@ internal class Program
         Console.WriteLine($"Part 1: {part1}");
     }
 
+    private static void Part2(ProgramInput input)
+    {
+        int y = 0;
+
+        long sum = 0;
+
+        foreach(string line in input.Grid)
+        {
+            for(int i = 0; i < line.Length; i++)
+            {
+                // Check if there is a possible gear connection
+                if (line[i] == '*')
+                {
+                    (string num1, string num2) = adjacentPartsNumbers(input, (i, y));
+                    
+                    // We might reach cases where there is not two adjacent part numbers
+                    // then its not a gear
+                    if (num1 == String.Empty || num2 == String.Empty)
+                    {
+                        continue;
+                    }
+
+                    sum += long.Parse(num1) * long.Parse(num2);
+                }
+            }
+
+            y++;
+        }
+
+        Console.WriteLine($"Part 2: {sum}");
+    }
+
+    private static (string, string) adjacentPartsNumbers(ProgramInput input, (int, int) source)
+    {
+        (int x, int y) = source;
+        IList<(int, int)> adj = new List<(int, int)>();
+        var withinBounds = Utils.WithinBounds(input.GridX, input.GridY);
+
+        // Find all adjacent numbers around source
+        for(int i = x - 1; i <= x + 1; i++)
+        {
+            for(int j = y - 1; j <= y + 1; j++) {
+                if (withinBounds((i, j)) && Char.IsDigit(input.Grid[j][i])) {
+                    adj.Add((i, j));
+                }
+            }
+        }
+
+        IList<((int, int), string)> numbersInScope = new List<((int, int), string)>();
+        foreach((int, int) a in adj)
+        {
+            numbersInScope.Add(seekPartsNumber(input, a));
+        }
+
+        // We risk having duplicate numbers, so we ask for the distrinct
+        // positions and numbers
+        var res = numbersInScope.Distinct().ToList();
+        if (res.Count() != 2)
+        {
+            return (string.Empty, string.Empty);
+        }
+
+        return (res[0].Item2, res[1].Item2);
+    }
+
+    /// seekPartsNumber returns the beginnign position of a number and the
+    /// number in string representation
+    private static ((int, int), string) seekPartsNumber(ProgramInput input, (int, int) source)
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        (int x, int y) = source;
+
+        while(x > 0 && Char.IsDigit(input.Grid[y][x]))
+        {
+            // We move the cursor to the left
+            x--;
+        }
+        // If we accidentally moved the curser beyond the number, we move it back one spot
+        if (!Char.IsDigit(input.Grid[y][x])) 
+        {
+            x++;
+        }
+
+        (int, int) pos = (x, y);
+        while(x < input.GridX && Char.IsDigit(input.Grid[y][x]))
+        {
+            // We eat up all the digits until we reach an end to tne number or 
+            // we run out of space on the grid
+            sb.Append(input.Grid[y][x++]);
+        }
+
+        return (pos, sb.ToString());
+    }
+
     private static void Main(string[] args)
     {
-        var input = _readInput(args);
+        if (args.Length != 2) {
+            Console.WriteLine("Usage: dotnet run <input_file>");
+            System.Environment.Exit(1);
+        }
 
+        var input = _readInput(args);
         Part1(input);
+        Part2(input);
     }
 
 }
